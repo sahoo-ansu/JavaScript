30928a292c95303b18180728ebb5b27d7610b05d
Ansuman Sahoo <ansumansahoo758@gmail.com>
2025-06-29 12:27:10 +0000

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountID=12345;
+let accEmail="ansuman@gmail.com"
+var accPass=4321
+accCity="KDP"
+let accountState;
+
+// accountID=123 //not changable
+
+accEmail="ansu@gmail.com"
+accPass=432
+accCity="Bbsr"
+
+console.log(accountID)
+console.table([accountID,accEmail,accPass,accCity,accountState])
+console.log(accountState)
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js code as newer version
+
+// alert(3+3) //we are using nodejs,not browser
+
+let name="ansuman"
+let age=18
+let isLoggedIn=false
+
+//number
+//bigint
+//string
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+//object
+
+console.log(typeof "ansuman");
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
