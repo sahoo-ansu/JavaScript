6620dbaffbbd449e90cdd1c0f67a2d9a59e5b3fe
Ansuman Sahoo <ansumansahoo758@gmail.com>
2025-06-29 12:57:35 +0000

    Conversion of Datatypes

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -5,6 +5,10 @@ accCity="KDP"
 let accountState;
 
 // accountID=123 //not changable
+/*
+Prefer not to use var
+Because of issue in block scope and functional scope
+*/
 
 accEmail="ansu@gmail.com"
 accPass=432
---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score="33abc"
+
+// console.log(typeof(score))
+
+let valInNumber=Number(score)
+
+// console.log(typeof(valInNumber));
+// console.log(valInNumber)
+
+//"33" => 33
+//"33abc" => NaN
+//true => 1; false => 0;
+
+let isLoggedIn="1";
+
+let boolIsLoggedIn=Boolean(isLoggedIn)
+
+console.log(typeof boolIsLoggedIn)
+console.log(boolIsLoggedIn)
+
+//1 => true; 0 => false
+//"" => false
+//"ansuman" => true
+//"0" => true; "1" => true; 
+// 0 => false
+
