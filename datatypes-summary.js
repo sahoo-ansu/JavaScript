83d3de6c2f189a00d3318d75055d619c523f90c5
Ansuman Sahoo <ansumansahoo758@gmail.com>
2025-06-30 12:02:17 +0000

    datatype summary

---
+++ datatypes-summary.js
@@ -0,0 +1,32 @@
+// primitive
+
+// 7 types : String,Number,Boolean,Null,Undefined,Symbol,BigInt
+
+const score=123
+const scoreVal=123.12
+
+const isLoggedIn=true
+const outsideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id===anotherId); //false
+
+
+// Reference (Non-primitive)
+
+// Array,Objects,Functions
+
+
+const heros=["superman" , "shaktiman" , "Krish"];
+let MyObj={
+    name:"Ansuman",
+    age : 21
+}
+
+let MyFunc=function(){
+    console.log("Hello World!!");
+    
+}
