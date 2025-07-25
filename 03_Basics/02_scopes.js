56e8f4ba43327fd64a6102279bf3f10096188c44
Ansuman Sahoo <ansumansahoo758@gmail.com>
2025-07-25 12:42:56 +0000

    scope 2

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -31,7 +31,45 @@ console.log("GLOBAL a:",a);     //GLOBAL a: 500
 Block of web-browser(inspect->console) different from block of node(code environment like in vs-code)
 */
 
+//closure
+
+function one(){
+    const username="ansuman"
+    function two(){
+        const website="youtube" 
+        console.log(username);    //ansuman
+    }
+    // console.log(website);    //it gives error
+
+    two()
+}
+
+one()
 
 
+if (true) {
+    const username="ansuman"
+    if (username=="ansuman") {
+        const website=" youtube"
+        console.log(username+website);    //ansuman youtube
+    }
+    // console.log(website); //it gives error
+}
+// console.log(username);   //it gives error
 
 
+//++++++++++++++++++++++++++++++++ Intresting ++++++++++++++++++++++++++++++++++++
+
+
+console.log(addOne(5))      //6             //before initialization we can access the function if we declare like this 
+function addOne(num){
+    return num+1                             
+}
+console.log(addOne(5))      //6
+
+
+// console.log(addTwo(5))        //error     // Cannot access 'addTwo' before initialization 
+const addTwo=function(num){      
+    return num+2
+}
+console.log(addTwo(5))      //7
